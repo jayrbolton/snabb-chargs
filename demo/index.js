9bdb478af892a66fdd7f775e1f0dc96e0edf97d4
Jay R Bolton <jayrbolton@gmail.com>
2016-07-11 15:28:27 -0700

    add data update handling

--- demo/index.js
+++ demo/index.js
@@ -4,49 +4,46 @@ import snabbdom from 'snabbdom'
 import h from 'snabbdom/h'
 
 function init() {
-  return {}
-}
-
-let chartOpts = {
-  type: 'bar',
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
+  return {
+    chartData: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+    , datasets: [{
+        label: '# of Votes'
+      , backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
           'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
+          'rgba(255, 159, 64, 0.2)'
+        ]
+      , borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true
-        }
+          'rgba(255, 159, 64, 1)'
+        ]
+      , data: [12, 19, 3, 5, 2, 3]
+      , borderWidth: 1
       }]
     }
   }
 }
 
+let chartOpts = {
+  type: 'bar',
+  options: {
+    scales: { yAxes: [{ ticks: { beginAtZero:true } }] }
+  }
+}
+
+let sampleChart = chart(chartOpts)
+
 function view(state) {
   return h('body', [
-    chart(chartOpts)
+    sampleChart(state.chartData)
   ])
 }
 
--- index.es6
+++ index.es6
@@ -2,15 +2,24 @@
 import h from 'snabbdom/h'
 import Chart from 'chart.js'
 
-
 module.exports = function(options) {
-  return h('canvas', {
-    hook: {
-      insert: vnode => {
-        debugger
-        new Chart(vnode.elm, options)
+  let chart = null
+  options.data = {datasets: [{label: '', data: []}], labels: ['']}
+  return function(newData) {
+    return h('canvas', {
+      hook: {
+        insert: vnode => {
+          chart = new Chart(vnode.elm, options)
+          for(var key in newData) { chart.data[key] = newData[key] }
+          chart.update()
+        }
+      , update: vnode => {
+          for(var key in newData) { chart.data[key] = newData[key] }
+          chart.update()
+        }
       }
-    }
-  }, ['hi'])
+    })
+  }
 }
+
 
